a8c5967cbf9b132c36a33e13de4ca375d3e58c0d
YordiDR <yordi.derijcke@live.be>
2026-04-23 22:40:30 +0200

    aa55_inverter: Replace std::to_string in favor of str_sprintf to avoid heap fragmentation

--- esphome/components/aa55_inverter/text_sensor/aa55_inverter_text_sensor.cpp
+++ esphome/components/aa55_inverter/text_sensor/aa55_inverter_text_sensor.cpp
@@ -67,7 +67,7 @@ void AA55InverterTextSensor::parse_ascii_payload(const std::vector<uint8_t> &pay
 void AA55InverterTextSensor::parse_work_mode_payload(const std::vector<uint8_t> &payload) {
   uint32_t work_mode_code = this->parse_int(payload);
   if (work_mode_code > 2) {
-    this->publish_state("Unknown: " + std::to_string(work_mode_code));
+    this->publish_state("Unknown: " + str_sprintf("%d", work_mode_code));
   } else {
     this->publish_state(aa55_inverter::WORK_MODE_LIST[work_mode_code]);
   }
